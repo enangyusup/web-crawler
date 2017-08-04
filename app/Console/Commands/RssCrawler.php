6a4562c4b2ea3e8901a8281b20d425412e741a67
Enang Yusup <enang.yusup@gmail.com>
2017-08-04 21:39:59 +0700

    Update rss crawler can save to database

--- app/Console/Commands/RssCrawler.php
+++ app/Console/Commands/RssCrawler.php
@@ -3,6 +3,8 @@ namespace App\Console\Commands;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Illuminate\Console\Command;
+use App\Models\Article;
+use Carbon\Carbon;
 
 class RssCrawler extends Command
 {
@@ -19,8 +21,16 @@ class RssCrawler extends Command
         $url = $this->argument('url');
         $rss_file = file_get_contents($url);
         $crawler = new Crawler($rss_file);
-        $crawler->filterXPath('//channel/item')->each(function($node){
-            $this->info($node->filterXPath('//link')->text());
+        $crawler->filterXPath('//channel/item')->each(function($node) {
+            $article = new Article;
+            $article->title        = trim($node->filterXPath('//title')->text());
+            $article->source_url   = $node->filterXPath('//link')->text();
+            $article->source_id    = $node->filterXPath('//guid')->text();
+            $article->content      = $node->filterXPath('//description')->text();
+            $article->publish_date = new Carbon($node->filterXPath('//pubDate')->text());
+            $article->save();
+
+            $this->info($article->title . ' saved');
         });
         $this->info($url . ' executed');
     }
